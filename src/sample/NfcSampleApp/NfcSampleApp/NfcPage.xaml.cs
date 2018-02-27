8e02e447acb7f7a463fae2c7d6459423325b7f65
onikiri2007 <onikiri2007@gmail.com>
2018-02-27 16:31:49 +1100

    fix project

---
+++ src/sample/NfcSampleApp/NfcSampleApp/NfcPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Nfc;
+using System.Threading;
+
+namespace NfcSampleApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NfcPage : ContentPage
+	{
+        private CancellationTokenSource _cancelSource;
+        private INfcDefTag _tag;
+        private DefTag _defTag = new DefTag();
+		public NfcPage ()
+		{
+			InitializeComponent ();
+             this.BindingContext = _defTag;
+            NfcTagList.ItemsSource = _defTag.Records;
+
+            CrossNfc.Current.TagDetected += Current_TagDetected;
+
+            this.Button.Command = new Command(async () =>
+            {
+               await CrossNfc.Current.StartListeningAsync();
+            });
+
+            this.WriteButton.Command = new Command(async () =>
+            {
+
+
+            });
+		}
+
+        private void Current_TagDetected(INfcDefTag tag)
+        {
+            _tag = tag;
+
+            _defTag.IsWritable = _tag.IsWriteable;
+            _defTag.TagId = _tag.TagId;
+            
+            var records = new List<Record>();
+            foreach(var record  in _tag.Records)
+            {
+                records.Add(new Record()
+                {
+                    TypeNameFormat = record.TypeNameFormat.ToString(),
+                    Payload =  System.Text.Encoding.Default.GetString(record.Payload)
+                });
+            }
+        }
+
+        protected async override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CrossNfc.Current.TagDetected -= Current_TagDetected;
+            await CrossNfc.Current.StopListeningAsync();
+
+        }
+    }
+
+     public class Record : ObservableObject
+    {
+        private string _typeFormat;
+        public string TypeNameFormat
+        {
+            get => _typeFormat;
+            set => SetProperty(ref _typeFormat, value);
+        }
+
+        private string _payload;
+        public string Payload
+        {
+            get => _payload;
+            set => SetProperty(ref _payload, value);
+        }
+    }
+
+   
+    public class DefTag : ObservableObject
+    {
+        public DefTag()
+        {
+            Records = new ObservableRangeCollection<Record>();
+        }
+
+        private string _tagId;
+        public string TagId
+        {
+            get => _tagId;
+            set => SetProperty(ref _tagId, value);
+        }
+
+        private bool _isWritable;
+        public bool IsWritable
+        {
+            get => _isWritable;
+            set => SetProperty(ref _isWritable, value);
+        }
+
+        public ObservableRangeCollection<Record> Records {get;}
+
+    }
+}
