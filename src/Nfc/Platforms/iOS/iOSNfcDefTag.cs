bcb2f6196d911df8548285be691690a0e78ac0be
onikiri2007 <onikiri2007@gmail.com>
2018-04-04 16:14:56 +1000

    ios

--- src/Nfc/Platforms/iOS/iOSNfcDefTag.cs
+++ src/Nfc/Platforms/iOS/iOSNfcDefTag.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreNFC;
+using Foundation;
 
 namespace Plugin.Nfc
 {
@@ -32,5 +33,47 @@ namespace Plugin.Nfc
         {
            
         }
+    }
+
+    public class iOSMifareUltraLightTag : INfcTag
+    {
+        public string TagId => null;
+        public bool IsWriteable => false;
+        public bool HasNfcDefRecords => Records != null && Records.Length > 0;
+        public NfcDefRecord[] Records { get; }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task<bool> WriteMessage(NfcDefMessage message)
+        {
+            return Task.FromResult(true);
+        }
+
+        public static NFCNdefMessage GetMessage(IEnumerable<byte[]> data)
+        {
+            var array = data.ToArray();
+            var r = ByteArrayExtensions.Combine(array);
+            var control = array[0];
+            var ln = (int)control[0];
+            var t = Convert.ToChar(control[1]);
+
+            if (t == NfcRecordTypeConstants.RTD_MIFAREULTRALIGHT_NDEF_MESSAGE)
+            {
+                var x = new byte[ln];
+                Buffer.BlockCopy(r, 4, x, 0, ln);
+                return new NFCNdefMessage(new NSCoder())
+                {
+                    Records = GetNdefRecordFromData(x)
+                };
+            }
+
+            return null;
+
+        }
+
+        private static NFCNdefPayload[] GetNdefRecordFromData(byte[] x) => throw new NotImplementedException();
     }
 }
